812befec31ee45af6eba87c90e2e3bdd6bac031d
azarniewicz <a.zarniewicz@gmail.com>
2020-06-06 16:35:09 +0200

    polskie wiadomosci błędów

---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Hasło zresetowane!',
+    'sent' => 'Wysłaliśmy wiadomość z linkiem do resetowania!',
+    'throttled' => 'Poczekaj chwilę przed następną próbą.',
+    'token' => 'Token resetu hasła jest nieprawidłowy.',
+    'user' => 'Nie znaleźliśmy użytkownika z tym adresem email.',
+
+];

84d5d5e20efd06fa71b49ac187bc0d5836f641d3
azarniewicz <a.zarniewicz@gmail.com>
2020-06-08 19:40:01 +0200

    usuwam reczny import bootstrapa bo mi psuje historie przelewów

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>STRONA GŁÓWNA</title>
+
     <meta name="description" content="DEMO BANKU - Żukowski, Męczyński, Żarniewicz">
     <meta name="author" content="Żukowski, Żarniewicz, Męczyński">
+
     <meta http-equiv="X-Ua-Compatible" content="IE=edge">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-</head>
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="js/bootstrap.min.js"></script>
- <script src="https://kit.fontawesome.com/285fa7da50.js" crossorigin="anonymous"></script>
- <script src="{{asset('js/app.js')}}"> </script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://kit.fontawesome.com/285fa7da50.js" crossorigin="anonymous"></script>
+    <script src="{{asset('js/app.js')}}"> </script>
 
+</head>
 <body>
 <div id="app">
     @include('layouts.navbar')
